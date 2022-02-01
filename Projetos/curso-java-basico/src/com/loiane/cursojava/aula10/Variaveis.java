a2fec339c045d5980070ec992a9e4bca23a168d9
Daniel Souza Filho <danieldsfilho@gmail.com>
2022-02-01 16:35:26 -0300

    variaveis

---
+++ Projetos/curso-java-basico/src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int idade = 18;
+		String nome = "Daniel";
+		String nomeDoMeuCachorro = "Charlie";
+		System.out.println("Idade:" + idade + "\n" +
+						   "Nome:"+ nome + "\n" + 
+						   "Cachorro:" + nomeDoMeuCachorro);
+
+	}
+
+}
